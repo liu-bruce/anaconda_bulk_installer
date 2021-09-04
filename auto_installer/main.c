9dce97fd4cb71fa681b19db23361e1779162f063
liu-bruce <87771104+liu-bruce@users.noreply.github.com>
2021-09-04 06:31:59 -0600

    Update main.c
    atom installer doesn't work

--- auto_installer/main.c
+++ auto_installer/main.c
@@ -10,9 +10,8 @@ void main()
   system("conda install -c cyclus java-jdk -y");
   //gcc install
   system("conda install -c conda-forge m2w64-gcc -y");
-  //git instakk
+  //git install
   system("conda install -c anaconda git -y ");
-  // atom text editor
-  system("conda install -c conda-forge atom -y");
+
 
 }
